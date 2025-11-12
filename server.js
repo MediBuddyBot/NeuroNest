f3da602a4d2cf99a775e1a9e721565f3c7d4780c
MediBuddyBot <medibuddy.bot@gmail.com>
2025-11-13 00:27:34 +0530

    Final dependency cleanup before Render deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { GoogleGenAI } = require('@google/genai');
 
 // Initialize Google GenAI client
